98f1c01f7aaba2d2ed8239864a85036de9cc62de
ExpoPythonist <ashique00003@gmail.com>
2024-05-02 23:35:21 +0600

    Note app initialized

---
+++ backend/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+    path("notes/edit/<int:pk>/", views.NoteUpdate.as_view(), name="update-note"),
+]
---
+++ frontend/src/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/',
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
